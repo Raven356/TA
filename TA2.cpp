af3dfd2d8229432eab38fd62e5554dec92c4b9cd
Raven356 <71709087+Raven356@users.noreply.github.com>
2021-03-03 15:06:09 +0200

    Update TA2.cpp

--- TA2.cpp
+++ TA2.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <conio.h>
+#include <cstdlib>
+#include <ctime>
 
 struct graf{
 int stepeni;
@@ -11,44 +13,75 @@ using namespace std;
 void ShowGraf(graf arr[22],int n);
 int CheckColor(graf arr[22],int n);
 void InitGraf(graf arr[22],int n);
-
+int m[20000][20000] = {};
 int main()
 {
     do
     {
+    system("cls");
     int color1[]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
 
-    struct graf arr[80];
+    struct graf arr[20000];
 
 
-    int m[80][80] = {};
+            for(int i = 0; i < 20000; i++)
+                for(int j = 0; j < 20000; j++)
+            {
+                m[i][j] = 0;
+            }
             cout << "Enter amount of tops of graf\n";
             int n{};
             cin >> n;
             cout << "Enter amount of connections\n";
             int con{};
             cin >> con;
+            cout << "1 - rand\t2 - mainualy\n";
+            int sw{};
+            cin >> sw;
+            if(sw == 2)
             cout << "Enter connections\n";
             int x1{}, y1{};
-            for(int i = 0; i < con; i++)
+            if (sw == 1)
+                srand(time(0));
+            int itter{};
+            for(int i = 0; i < con; i++, itter++)
             {
-                cin >> x1 >> y1;
+                if(sw == 2)
+                    cin >> x1 >> y1;
+                if(sw == 1)
+                    {
+                        if(itter > 0)
+                            srand(itter + 1);
+                        x1 = rand() % (n-1) + 1;
+                        y1 = rand() % (n-1) + 2;
+                        if(x1 == y1 && (y1 < n-1))
+                           y1++;
+                        if(x1 == y1 && (y1 > n-1 && y1 > 2))
+                            y1--;
+                    }
                 m[x1 - 1][y1 - 1] = 1;
                 m[y1 - 1][x1 - 1] = 1;
+                if(x1 == y1)
+                {
+                    m[x1 - 1][y1 - 1] = 0;
+                    m[y1 - 1][x1 - 1] = 0;
+                    i--;
+                }
             }
-
+    if(n < 100)
     for(int i = 0; i < n; i++)
     {
-        for(int j = 0; j < n; j++)    //âèâ³ä ìàòðèö³
+        for(int j = 0; j < n; j++)    //вивід матриці
         cout << m[i][j] << " ";
     cout << endl;
     }
     cout << endl;
     int versh=0;
 
-    InitGraf(arr,n);//³í³ö³àë³çàö³ÿ êîëüîðó çà çíà÷åííÿì 66
+    auto start = clock();
+    InitGraf(arr,n);//ініціалізація кольору за значенням 66
 
-//íàõîäèìî ñòåïåí³ âåðøèí
+//находимо степені вершин
     for(int i=0; i < n; i++,versh++)
         {
             arr[versh].stepeni=0;
@@ -58,7 +91,7 @@ int main()
                     arr[versh].stepeni++;
         }
 
-//ñîðòóâàííÿ ïî ñòåïåíÿì â³ä íàéá³ëüøîãî äî íàéìåíøîãî
+//сортування по степеням від найбільшого до найменшого
     struct graf temp;
 
         for(int i=0; i < n; i++)
@@ -70,18 +103,18 @@ int main()
                 arr[j]=temp;
             }
 
-   //àëãîðèòì ïðèñâîºííÿ êîëüîðó
+   //алгоритм присвоєння кольору
     int x=0,t,i,j,flag=0,metk=0,s;
 
-    for(x=0; CheckColor(arr,n)!=0 ; x++)  //öèêë ïðèñâîºííÿ êîëüîðó äîêè âñå íå îêðàøåíî
+    for(x=0; CheckColor(arr,n)!=0 ; x++)  //цикл присвоєння кольору доки все не окрашено
     {
-        if(arr[x].color == 66) arr[x].color = color1[x];      //ïðèñâîºííÿ ïåðøîãî êîëüîðó
+        if(arr[x].color == 66) arr[x].color = color1[x];      //присвоєння першого кольору
 
-        for(i=0; i < n; i++) //öèêë äëÿ ðîçôàðáóâàííÿ óñ³õ íåñì³æíèõ âåðøèí
+        for(i=0; i < n; i++) //цикл для розфарбування усіх несміжних вершин
         {
-            t=arr[i].number; //íîìåð ñòðîêè ìàòðèö³ ñóì³æíîñò³
+            t=arr[i].number; //номер строки матриці суміжності
             flag=0;
-            for(j=0; j < n; j++)    //³äåìî ó ñòðîêó ìàòðèö³ ïî íîìåðó
+            for(j=0; j < n; j++)    //ідемо у строку матриці по номеру
             {
                 if((m[t][j] == 1 && t!=j))
                 {
@@ -99,7 +132,9 @@ int main()
             }
        }
   }
+  auto stop = clock();
     ShowGraf(arr,n);
+      cout << "Time = " << (stop-start) / 1e5 << endl;
     }
     while(getch() != 27);
     return 0;
